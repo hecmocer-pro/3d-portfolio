ed55cdc166f89f812568577851e9260d2f5f3da2
Hector Moreno <>
2020-10-18 18:02:34 +0200

    updated clock to go faster

--- src/scripts/animate.js
+++ src/scripts/animate.js
@@ -5,7 +5,7 @@ export default function({ physics, renderer, scene, camera, animationCallback })
     const animate = () => {
 
         // physics.update(clock.getDelta() * 1000)
-        physics.update(clock.getDelta() * 3000)
+        physics.update(clock.getDelta() * 8000)
         physics.updateDebugger()
         renderer.render(scene, camera)
 
